6746b03bbaa5f3973999451075676b9f991d7956
Abhijeet Kumar <80936610+Abhijeetbyte@users.noreply.github.com>
2023-05-27 10:40:46 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>About US</title>
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+</head>
+
+<body>
+
+
+
+    <header class="text-gray-600 body-font">
+        <div class="container mx-auto flex flex-wrap p-5 flex-col
+                md:flex-row
+                items-center">
+
+            <a class="flex title-font font-medium items-center text-gray-900
+                    mb-4
+                    md:mb-0">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round"
+                    stroke-linejoin="round" stroke-width="2" class="w-10
+                        h-10 text-white
+                        p-2 bg-blue-500 rounded-full" viewBox="0 0 24 24">
+                    <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2
+                            12l10 5 10-5"></path>
+                </svg>
+                <span class="ml-3 text-xl">Arsenic Project (MIS)</span>
+            </a>
+
+            <nav class="md:ml-auto flex flex-wrap items-center text-base
+                    justify-center">
+                  <button  onclick="window.location.href='index.php';" class="mr-5 inline-flex items-center bg-blue-500 border-0 py-1 px-3 focus:outline-none hover:bg-blue-900 rounded text-white text-base mt-4 md:mt-0">Back
+                </button>
+            </nav>
+
+        </div>
+    </header>
+
+
+    <section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
+
+      <p class="leading-relaxed text-lg">
+        
+The Magnetic Arsenic Removal Unit is a technology developed for the purpose of removing arsenic from contaminated water sources. It utilizes an indigenous molecular magnetic repulsion technology to eliminate arsenic from water. 
+
+The primary objective of the development of this technology is to tackle the severe problem of arsenic contamination in drinking water sources. It has undergone two years of extensive pilot testing to evaluate its effectiveness in removing arsenic from contaminated water sources. During this period, the technology has shown promising results and has demonstrated its potential as a sustainable solution for communities facing arsenic contamination in their water supply.</p>
+
+      <span class="inline-block h-1 w-10 rounded bg-blue-500 mt-8 mb-6"></span>
+      <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">Arpit Kumar</h2>
+      <p class="text-gray-500">Project Lead</p>
+    </div>
+  </div>
+</section>
+
+<section>
+    <div class="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+      <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+        <h2 class="title-font font-medium text-blue-900 tracking-widest text-sm mb-3">CONTACT</h2>
+        <nav class="list-none mb-10">
+          <li>
+            <a href="mailto:arpitpatna3@gmail.com" class="text-gray-600 hover:text-gray-800">Email</a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/arpit-kumar-i/" target="_blank" class="text-gray-600 hover:text-gray-800">Linkedin</a>
+          </li>
+          <li>
+            <a href="https://kilkaribihar.in/" target="_blank" class="text-gray-600 hover:text-gray-800">Website</a>
+          </li>
+        </nav>
+      </div>
+</section>
+
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+
+
+<section class="text-gray-600 body-font">
+  <div class="container px-5 py-10 mx-auto">
+    <div class="flex flex-col text-center w-full mb-20">
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">‘Kilkari’ Bihar Bal Bhawan Saidpur, Patna 800004, Bihar (India)</p>
+    </div>
+ </div>
+</section>
+
+
+
+    <footer class="text-white body-font bg-blue-500">
+        <div class="container px-5 py-3 mx-auto flex items-center flex-col">
+            <p class="text-1xl text-white sm:ml-4 sm:pl-4 sm:py-2 sm:mt-0
+                    mt-4">
+                Copyright © 2023 Arsenic Quality Logger. designed by
+                <a href="http://www.kilkaribihar.in/" class="text-white
+                        ml-1" rel="noopener noreferrer" target="_blank">Bihar Bal Bhawan Kilkari</a>
+            </p>
+        </div>
+    </footer>
+
+
+
+
+</body>
+
+</html>
